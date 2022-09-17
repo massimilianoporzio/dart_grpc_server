cd163138cb9e5e620a0821fd02371aa13fbb72af
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-09-17 15:31:56 +0200

    fixed main

--- bin/dart_grpc_server.dart
+++ bin/dart_grpc_server.dart
@@ -1,5 +1,3 @@
 import 'package:dart_grpc_server/dart_grpc_server.dart' as dart_grpc_server;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_grpc_server.calculate()}!');
-}
+void main(List<String> arguments) {}
