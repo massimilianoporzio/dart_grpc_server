082b08e94f6e751ccf4716a43b46111f487d7afa
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-09-17 15:31:29 +0200

    fixed

--- test/dart_grpc_server_test.dart
+++ test/dart_grpc_server_test.dart
@@ -1,8 +1,4 @@
 import 'package:dart_grpc_server/dart_grpc_server.dart';
 import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+void main() {}
