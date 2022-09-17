998044942adf538b5b006d8e7123c2fcab0bcdb5
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-09-17 15:30:49 +0200

    implementazione metodi da usare

---
+++ lib/src/helper/methods.dart
@@ -0,0 +1,33 @@
+import 'package:dart_grpc_server/dart_grpc_server.dart';
+
+class HelperMethods {
+  Category getCategoryFromMap(Map category) {
+    var _idTag = 1;
+    var _nameTag = 2; //COME IN PROTO FILE
+    //legate alla struttura mappa nei dati
+    int _id = category['id']; //prendo 'id' dalla mappa in input
+    String _name = category['name'];
+    return Category.fromJson('''{
+      "$_idTag": $_id,
+      "$_nameTag":"$_name"}
+      ''');
+  }
+
+  Item getItemFromMap(Map item) {
+    var _idTag = 1;
+    var _nameTag = 2;
+    var _categoryIdTag = 3;
+
+    int _id = item['id'];
+    String _name = item['name'];
+    int _categoryId = item['categoryId'];
+
+    return Item.fromJson('''{
+      "$_idTag" : $_id,
+      "$_nameTag": "$_name",
+      "$_categoryIdTag":$_categoryId
+    }''');
+  }
+}
+
+final helper = HelperMethods();
